d62f60cd699db2f495aa09c77c03680050b4f5fc
Maddie DeVoe <mdevoe@Maddies-MacBook-Pro.local>
2020-10-22 12:21:18 -0500

    playing with vibration patterns

--- components/test.js
+++ components/test.js
@@ -1,104 +1,117 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { TouchableOpacity, Text, StyleSheet, View, Image, Vibration } from 'react-native';
-import {emojinames, imageDict, patternDict, randomEmoji } from '../sharedVars.js';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  View,
+  Image,
+  Vibration,
+} from "react-native";
+import {
+  emojinames,
+  imageDict,
+  patternDict,
+  randomEmoji,
+} from "../sharedVars.js";
+
+// to do
+// randomize emoji display order
+// implement sending test
+// first is always wrong?
 
 class Test extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        correctEmoji: 'angry',
-        numTries: 0,
-        text: '',
-        trials: 0,
-        data: []
-      };
-    };  
-
-    render() {
-        if (this.props.display) {
-            return(
-              <View>
-                {emojinames.map((name) => (
-                  <TouchableOpacity style={styles.cont} onPress={() => this.checkCorrect(name)}>
-                      <Image style={styles.img} source={eval(imageDict[name])} />
-                  </TouchableOpacity>
-                ))}
-                <Text style={styles.text}>{this.state.text}</Text>
-              </View>
-            );
-        }
-        else {return(<View></View>)}
+  constructor(props) {
+    super(props);
+    this.state = {
+      correctEmoji: "angry",
+      numTries: 0,
+      text: "",
+      trials: 0,
+      data: [],
     };
+  }
 
-
-    //prepares state for next trial, and sends vibration of emoji argument
-    //unless max number of trials has been reached, then displays the results and exits to introscreen
-    sendVibration = (emoji) => {
-      if (this.state.trials < 5) {
-        this.setState({
-          correctEmoji: emoji,
-          numTries: 0,
-          text: 'sending vibration... (trial ' + (this.state.trials+1) + ')',
-          trials: this.state.trials + 1
-        });
-        //console.log(eval(patternDict[emoji+this.props.testType]));
-        Vibration.vibrate(eval(patternDict[emoji+this.props.testType]));
-      }
-      else {
-        console.log('RESULTS OF TEST ' + this.props.testType);
-        console.log(this.state.data)
-        this.setState({
-          data: [],
-          trials: 0
-        });
-        this.props.exit();
-      }
+  render() {
+    if (this.props.display) {
+      return (
+        <View>
+          {emojinames.map((name) => (
+            <TouchableOpacity
+              style={styles.cont}
+              onPress={() => this.checkCorrect(name)}
+            >
+              <Image style={styles.img} source={eval(imageDict[name])} />
+            </TouchableOpacity>
+          ))}
+          <Text style={styles.text}>{this.state.text}</Text>
+        </View>
+      );
+    } else {
+      return <View></View>;
     }
+  }
 
-    //called on press of one of the emojis, checks if it is correct 
-    //if it is correct, records the trial in state.data, 
-    //and initiates next trial by calling sendVibration
-    //if it isn't, increments numTries and waits for next attempt
-    checkCorrect = (emoji) => {
-      //console.log('correct: ' + this.state.correctEmoji);
-      if (emoji == this.state.correctEmoji) {
-        this.state.data.push([this.state.correctEmoji,this.state.numTries+1])
-        this.sendVibration(randomEmoji());
-      }
-      else {
-        this.setState({
-          numTries: this.state.numTries + 1,
-          text: 'incorrect(' + (this.state.numTries+1) + ' attempts), try again'
-        });
-      }
+  //prepares state for next trial, and sends vibration of emoji argument
+  //unless max number of trials has been reached, then displays the results and exits to introscreen
+  sendVibration = (emoji) => {
+    if (this.state.trials < 5) {
+      this.setState({
+        correctEmoji: emoji,
+        numTries: 0,
+        text:
+          "sending vibration... (trial " +
+          (this.state.trials + 1) +
+          ")" +
+          "emoji = " +
+          emoji,
+        trials: this.state.trials + 1,
+      });
+      //console.log(eval(patternDict[emoji+this.props.testType]));
+      Vibration.vibrate(eval(patternDict[emoji + this.props.testType]));
+    } else {
+      console.log("RESULTS OF TEST " + this.props.testType);
+      console.log(this.state.data);
+      this.setState({
+        data: [],
+        trials: 0,
+      });
+      this.props.exit();
     }
+  };
 
-    
-};
-
-  
-
+  //called on press of one of the emojis, checks if it is correct
+  //if it is correct, records the trial in state.data,
+  //and initiates next trial by calling sendVibration
+  //if it isn't, increments numTries and waits for next attempt
+  checkCorrect = (emoji) => {
+    //console.log('correct: ' + this.state.correctEmoji);
+    if (emoji == this.state.correctEmoji) {
+      this.state.data.push([this.state.correctEmoji, this.state.numTries + 1]);
+      this.sendVibration(randomEmoji());
+    } else {
+      this.setState({
+        numTries: this.state.numTries + 1,
+        text:
+          "incorrect(" + (this.state.numTries + 1) + " attempts), try again",
+      });
+    }
+  };
+}
 
 const styles = StyleSheet.create({
-    cont: {
-        alignItems: 'center',
-        padding:20
-    },
-    img: {
-      height:50,
-      width:50
-    },
-    text: {
-      fontWeight: 'bold',
-      padding:20
-    }
+  cont: {
+    alignItems: "center",
+    padding: 20,
+  },
+  img: {
+    height: 50,
+    width: 50,
+  },
+  text: {
+    fontWeight: "bold",
+    padding: 20,
+  },
 });
 
-
 export default Test;
-
-
-
-
-
--- sharedVars.js
+++ sharedVars.js
@@ -1,56 +1,59 @@
-export const emojinames = ['happy','angry','sad','love','laugh','like'];
+export const emojinames = ["happy", "angry", "sad", "love", "laugh", "like"];
 
 //returns a random element of emoji names
 export const randomEmoji = () => {
-    return emojinames[Math.floor(Math.random() * Math.floor(6))];
-}
+  return emojinames[Math.floor(Math.random() * Math.floor(6))];
+};
 
 //numbers are the time in ms between the vibrations, in order
 //on ios vibrations are fixed length, always 400ms
 //function kinda starts getting wierd when the separation times are low (<500)
-//ENCODING 1
-const happy1 = [600]
-const sad1 = [600,600]
-const angry1 = [600,600,600]
-const love1 = [600,600,600,600]
-const laugh1 = [600,600,600,600,600]
-const like1 = [600,600,600,600,600,600]
 
-//ENCODING 2
-const happy2 = [600,600,600,600,600,600]
-const sad2 = [600,600,600,600,600]
-const angry2 = [600,600,600,600]
-const love2 = [600,600,600]
-const laugh2 = [600,600]
-const like2 = [600]
+// ENCODING 1 - neutral encoding
+const happy1 = [600];
+const sad1 = [600, 600];
+const angry1 = [600, 600, 600];
+const love1 = [600, 600, 600, 600];
+const laugh1 = [600, 600, 600, 600, 600];
+const like1 = [600, 600, 600, 600, 600, 600];
+
+// ENCODING 2 - intuitive encoding
+const happy2 = [600, 600, 600];
+const sad2 = [800, 1200, 1600];
+// const sad2 = [600, 600, 600, 600, 600];
+const angry2 = [1200, 1200, 1200];
+// const angry2 = [600,600,600,600]
+const love2 = [600, 600];
+const laugh2 = [1200, 600, 600, 1200, 600, 600, 1200, 600, 600];
+const like2 = [600];
 
 export const patternDict = {
-    "angry1": angry1,
-    "sad1": sad1,
-    "happy1": happy1,
-    "laugh1": laugh1,
-    "like1": like1,
-    "love1": love1,
-    "angry2": angry2,
-    "sad2": sad2,
-    "happy2": happy2,
-    "laugh2": laugh2,
-    "like2": like2,
-    "love2": love2
-}
+  angry1: angry1,
+  sad1: sad1,
+  happy1: happy1,
+  laugh1: laugh1,
+  like1: like1,
+  love1: love1,
+  angry2: angry2,
+  sad2: sad2,
+  happy2: happy2,
+  laugh2: laugh2,
+  like2: like2,
+  love2: love2,
+};
 
 //emoji images
-const angryimg = require('./images/angry.jpeg');
-const sadimg = require('./images/sad.jpeg');
-const happyimg = require('./images/happy.jpeg');
-const laughimg = require('./images/laugh.png');
-const likeimg = require('./images/like.jpeg');
-const loveimg = require('./images/love.png');
+const angryimg = require("./images/angry.jpeg");
+const sadimg = require("./images/sad.jpeg");
+const happyimg = require("./images/happy.jpeg");
+const laughimg = require("./images/laugh.png");
+const likeimg = require("./images/like.jpeg");
+const loveimg = require("./images/love.png");
 export const imageDict = {
-    "angry": angryimg,
-    "sad": sadimg,
-    "happy": happyimg,
-    "laugh": laughimg,
-    "like": likeimg,
-    "love": loveimg
-}
+  angry: angryimg,
+  sad: sadimg,
+  happy: happyimg,
+  laugh: laughimg,
+  like: likeimg,
+  love: loveimg,
+};
